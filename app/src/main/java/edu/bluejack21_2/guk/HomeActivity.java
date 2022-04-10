aaede400b727769118a78530d36a7cae2158e59b
jevon <jevon.levin@gamil.com>
2022-04-10 14:52:08 +0700

    fix title bar

--- app/src/main/java/edu/bluejack21_2/guk/HomeActivity.java
+++ app/src/main/java/edu/bluejack21_2/guk/HomeActivity.java
@@ -6,15 +6,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.widget.TextView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
@@ -28,26 +23,20 @@ public class HomeActivity extends AppCompatActivity {
 
     private TextView welcomeTxt;
 
-    RecyclerView recyclerView;
-//    DatabaseReference database;
-    DogAdapter dogAdapter;
-    ArrayList<Dog> dogList;
-//    FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-
-
+    private RecyclerView recyclerView;
+    private DogAdapter dogAdapter;
+    private ArrayList<Dog> dogList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         welcomeTxt = findViewById(R.id.home_welcome_txt);
-        welcomeTxt.setText("Welcome, " + User.CURRENT_USER.getName() + " !");
+        String txt = "Hello, <b>" + User.CURRENT_USER.getName() + "</b> !";
+        welcomeTxt.setText(Html.fromHtml(txt));
 
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler1);
-//        database = FirebaseDatabase.getInstance().getReference("");
-//        Log.d("test", database.toString());
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -61,38 +50,11 @@ public class HomeActivity extends AppCompatActivity {
                     Dog dog = document.toObject(Dog.class);
                     Log.d("test", dog.getName());
                     dogList.add(dog);
-//                    for(int i =0; i<dogList.size(); i++){
-//                        Log.d("test", dogList.get(i).toString());
-//                    }
                     dogAdapter.notifyDataSetChanged();
                 }
             }
         });
 
-
-
-//        database.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//
-//                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
-//
-//                    Dog dog = dataSnapshot.getValue(Dog.class);
-//                    dogList.add(dog);
-//
-//                    for(int i =0; i<dogList.size(); i++){
-//                        Log.d("test", dogList.get(i).toString());
-//                    }
-//                }
-//                dogAdapter.notifyDataSetChanged();
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//
-//            }
-//        });
 
     }
 }
